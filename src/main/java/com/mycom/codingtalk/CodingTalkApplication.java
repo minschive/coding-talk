e8feb684eae9c1a0df3d9e1225b6c9f117cf5381
Jimin Byun <fraise3838@naver.com>
2025-04-23 10:11:01 +0900

    chore : init spring project

---
+++ src/main/java/com/mycom/codingtalk/CodingTalkApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.codingtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycom/codingtalk/CodingTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.codingtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
