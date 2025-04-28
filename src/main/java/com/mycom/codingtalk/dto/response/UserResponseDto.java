473b8276b26244212439e1a9d40645597e9f18ca
Jimin Byun <fraise3838@naver.com>
2025-04-28 16:32:52 +0900

    fix : 필드 원상복구

--- src/main/java/com/mycom/codingtalk/dto/response/UserResponseDto.java
+++ src/main/java/com/mycom/codingtalk/dto/response/UserResponseDto.java
@@ -3,14 +3,11 @@ package com.mycom.codingtalk.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 public class UserResponseDto {
     private Long id;
     private String email;
     private String name;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private String message;
 }
