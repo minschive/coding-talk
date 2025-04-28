242533f70ca39d4ea2be8db987d73fb8a7d38970
Jimin Byun <fraise3838@naver.com>
2025-04-28 16:02:00 +0900

    fix : 필드 변경

--- src/main/java/com/mycom/codingtalk/dto/response/UserResponseDto.java
+++ src/main/java/com/mycom/codingtalk/dto/response/UserResponseDto.java
@@ -3,11 +3,14 @@ package com.mycom.codingtalk.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class UserResponseDto {
     private Long id;
     private String email;
     private String name;
-    private String message;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
